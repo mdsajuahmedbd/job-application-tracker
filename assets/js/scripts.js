52774ccd86d5cc49f9663b38bf997aec54e080f3
mdsajuahmedbd <developersajuahmed@gmail.com>
2026-02-24 22:42:10 +0600

    Necessary Variables Declared

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("JS Linked")
+const totalApp = document.getElementById("total-app");
+const interviewApp = document.getElementById("interview-app");
+const rejectedApp = document.getElementById("rejected-app");
+
+const totalJobs = document.getElementById("total-jobs");
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+const interviewFilterBtn = document.getElementById("interview-filter-btn");
+const rejectedFilterBtn = document.getElementById("rejected-filter-btn");
+
+const allJobs = document.getElementById("all-jobs");
+const noJobs = document.getElementById("no-jobs");
+
+let currentFilter = "all"; // Default Filter: All
